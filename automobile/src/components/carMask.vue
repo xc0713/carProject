a2ffecfa781bad0b45d70a3593f41c719e881536
xiaoc <1740454645@qq.com>
2019-07-15 17:06:31 +0800

    '完善目录'

---
+++ automobile/src/components/carMask.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="root">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style <style lang="scss" scoped>
+    #root{
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
--- automobile/src/views/Home.vue
+++ automobile/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      Home
+    <carMask/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import carMask from '@/components/carMask.vue'; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
   components: {
-    HelloWorld,
+   carMask,
   },
 });
 </script>
